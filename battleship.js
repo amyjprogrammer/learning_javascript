4eb908a8633518272023ab5708430e6a44c4b5c6
amyjprogrammer <amyjprogrammer@gmail.com>
2021-03-30 22:30:17 -0400

    Functions JS

--- battleship.js
+++ battleship.js
@@ -1,6 +1,8 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 
 var guess;
 var hits = 0;
@@ -13,7 +15,7 @@ while (isSunk == false) {
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
     } else{
-        guesses = guesses + 1;
+        guesses += 1;
     
         if (guess == location1 || guess == location2 || guess == location3){
             hits += 1;
