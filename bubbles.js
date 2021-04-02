2c25a03f75833e028c8df8fe85b4d7150494d48f
amyjprogrammer <amyjprogrammer@gmail.com>
2021-04-01 23:41:35 -0400

    Updating Bubbles

--- bubbles.js
+++ bubbles.js
@@ -5,10 +5,27 @@ var scores = [60, 50, 60, 58, 54, 54,
               46, 31, 57, 52, 44, 18, 
               41, 53, 55, 61, 51, 44];
 
+var highScore = 0;
 var output;
 
-for (var i = 0; i < scores.length; i += 1) {
+for (var i = 0; i < scores.length; i++) {
     output = "Bubble solution #" + i + " score: " + scores[i];
-    
+ 
     console.log(output);
+    if (scores[i] > highScore) {
+        highScore = scores[i];
+    }
 }
+
+console.log ("Bubbles tests: " + scores.length);
+console.log ("Highest bubble score: " + highScore);
+
+var bestSolutions = [];
+for (var i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions.push(i);
+    }
+}
+
+console.log ("Solutions with highest score: " + 
+        bestSolutions);
