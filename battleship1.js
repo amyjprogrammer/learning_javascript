a54634174ea4677e6168ed8707049c395e26aab3
amyjprogrammer <amyjprogrammer@gmail.com>
2021-04-09 01:51:34 -0400

    Adding Fire and Controller

--- battleship1.js
+++ battleship1.js
@@ -71,6 +71,7 @@ var model = {
 };
         
 //testing model
+/*
 model.fire("53"); // miss
 
 model.fire("16"); //hit
@@ -88,5 +89,87 @@ model.fire("45"); //miss
 model.fire("12"); // hit
 model.fire("11"); // hit
 model.fire("10"); // hit
+*/
+
+var controller = {
+    guesses: 0,
     
+    processGuess: function(guess) {
+        var location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            var hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage ("You sank all my battleships, in " +
+                                     this.guesses + " guesses");
+            }
+                                     
+        }
+    }
+};
+
+
+function parseGuess(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and number on the board.");
+    } else {
+        var firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+        
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+        
+    }
+    return null;
+};
+
+//testing parseGuess 
+/*
+console.log(parseGuess('A0'));
+console.log(parseGuess('B6'));
+console.log(parseGuess('G3'));
+console.log(parseGuess('H0'));
+console.log(parseGuess('A7'));
+*/
+
+//testing processGuess
+/*
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
+*/
+
+function init() {
+    var fireButton = document.getElementById("fireButton");
+    fireButton.onclick = handleFireButton;
+};
+
+function handleFireButton() {
+    var guessInput = document.getElementById("guessInput");
+    var guess = guessInput.value;
+    controller.processGuess(guess);
+    
+    guessInput.value = "";
+};
+
+window.onload = init;
+
+
+
+
     
